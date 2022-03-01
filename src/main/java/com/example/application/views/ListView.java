e092e710ba2acc32a37180df66aaf06f4a36f9ee
Matthew Tang <matttang27@gmail.com>
2022-02-28 23:00:52 -0500

    Added View / Edit task

--- src/main/java/com/example/application/views/ListView.java
+++ src/main/java/com/example/application/views/ListView.java
@@ -1,5 +1,6 @@
 package com.example.application.views;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 import com.example.application.views.code.*;
@@ -32,6 +33,7 @@ public class ListView extends VerticalLayout {
     private static Manager manager;
     private static TaskManager taskManager;
     private static GroupManager groupManager;
+    private static DateTimeFormatter defDTFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     public ListView() {
 
 
@@ -62,17 +64,19 @@ public class ListView extends VerticalLayout {
         task -> {
             Checkbox checkBox = new Checkbox();
             checkBox.setValue(task.getDone());
-            checkBox.addValueChangeListener(event -> task.setDone(event.getValue())); // inside setA() method you need to set isB and isC to false if the new value is true. No good!
+            checkBox.addValueChangeListener(event -> task.setDone(event.getValue())); 
             return checkBox;
         });
         grid.addColumn(Task::getName).setHeader("Name").setFrozen(true).setKey("name");
-        grid.addColumn(Task::getNextDueString).setHeader("Due").setKey("due");
+        //TODO: Change getNextDue format (preferably without an entire new function)
+        grid.addColumn(Task::getNextDue).setHeader("Due").setKey("due");
         grid.addColumn(Task::getPriority).setHeader("Priority").setKey("priority");
         GridContextMenu<Task> menu = grid.addContextMenu();
         menu.addItem("View", event -> {
             Task selectedTask = event.getItem().get();
             Dialog itemDialog = new Dialog();
-            itemDialog.add(viewItemLayout(itemDialog,selectedTask));
+            itemDialog.add(viewItemLayout(itemDialog,grid,selectedTask));
+            itemDialog.open();
         });
         //TODO: figure out how to color GridContextMenu options
         menu.addItem("Delete", event -> {
@@ -121,6 +125,7 @@ public class ListView extends VerticalLayout {
             }
             
             Task newTask = new Task(nameField.getValue(),dueField.getValue(), priorityField.getValue().intValue());
+            newTask.setLastEdited(LocalDateTime.now());
             taskManager.addTask(newTask);
             grid.setItems(taskManager.getTasks());
 
@@ -140,23 +145,66 @@ public class ListView extends VerticalLayout {
         return dialogLayout;
     }
 
-    private static VerticalLayout viewItemLayout(Dialog dialog,Task task) {
-        TextField title = new TextField();
-        title.setValue(task.getName());
-        title.addThemeName("font-24px");
+    private static VerticalLayout viewItemLayout(Dialog dialog,Grid grid,Task task) {
 
+        TextField nameField = new TextField("Task Name");
+        nameField.setValue(task.getName());
+        nameField.setRequiredIndicatorVisible(true);
+        nameField.setErrorMessage("This field is required");
         DateTimePicker dueField = new DateTimePicker("Due Time");
+        dueField.setValue(task.getNextDue());
         dueField.setRequiredIndicatorVisible(true);
         dueField.setErrorMessage("This field is required");
-        dueField.setValue(task.getNextDue());
         NumberField priorityField = new NumberField("Priority (1-10)");
+        priorityField.setValue((double) task.getPriority());
         priorityField.setRequiredIndicatorVisible(true);
         priorityField.setErrorMessage("This field is required");
-        priorityField.setValue((double) task.getPriority());
+        TextField createdField = new TextField("Date created");
+        createdField.setReadOnly(true);
+        createdField.setValue(task.getCreated().format(defDTFormat));
+        TextField editField = new TextField("Last edited");
+        editField.setValue(task.getLastEdited().format(defDTFormat));
+        editField.setReadOnly(true);
 
+        VerticalLayout fieldLayout = new VerticalLayout(nameField,dueField,priorityField,new HorizontalLayout(createdField,editField));
+
+        Button cancelButton = new Button("Cancel", e -> dialog.close());
+        Button saveButton = new Button("Save", e -> {
+            //checks whether any of the fields are empty, if true, sends a notification and cancels
+            String error = "";
+            if (nameField.getValue() == null || dueField.getValue() == null || priorityField.getValue() == null) {
+                error = "Fill in all required fields";
+            }
+            else if (priorityField.getValue().intValue() > 10 || priorityField.getValue().intValue() < 1) {
+                error = "Priority not in range";
+            }
+
+            if (!error.equals("")) {
+                Notification errorNotif = Notification.show(error);
+                errorNotif.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                errorNotif.setDuration(2000);
+                return;
+            }
+            
+            task.setName(nameField.getValue());
+            task.setNextDue(dueField.getValue());
+            task.setPriority(priorityField.getValue().intValue());
+            task.setLastEdited(LocalDateTime.now());
+            grid.setItems(taskManager.getTasks());
+
+            Notification addTaskNotif = Notification.show("Task Updated!");
+            addTaskNotif.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+            addTaskNotif.setDuration(2000);
+            dialog.close();
+            
+        });
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        HorizontalLayout buttonLayout = new HorizontalLayout(cancelButton,saveButton);
+        
+        VerticalLayout dialogLayout = new VerticalLayout(fieldLayout,buttonLayout);
 
         
-        VerticalLayout dialogLayout = new VerticalLayout();
         return dialogLayout;
     }
 }
